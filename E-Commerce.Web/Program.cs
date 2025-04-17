648c9d7ab27013a2ca3d6d73176b26ba85950bd6
shahdlu <117366018+shahdlu@users.noreply.github.com>
2025-04-17 18:31:29 +0200

    05 Product Configuration & DbContext

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -14,6 +17,10 @@ namespace E_Commerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             #endregion
 
